1e20591f00ccc7bf8e0e65af67e8be5f60d16e84
Giedrius Trumpickas <trumpyla@artagon.com>
2010-02-19 16:54:10 +0000

    Added VariableDefinition test case

--- xacml-core/src/test/java/com/artagon/xacml/v3/policy/XacmlPolicyTestCase.java
+++ xacml-core/src/test/java/com/artagon/xacml/v3/policy/XacmlPolicyTestCase.java
@@ -2,14 +2,19 @@ package com.artagon.xacml.v3.policy;
 
 import org.junit.Before;
 
+import static org.easymock.EasyMock.createStrictMock;
+
 public class XacmlPolicyTestCase
 {	
 	protected EvaluationContext context;
 	protected MockAttributeResolutionService attributeService;
+	protected Policy currentPolicy;
 	
 	@Before
 	public void init_XACMLTestCase() throws Exception{
 		this.attributeService = new MockAttributeResolutionService();
-		this.context = new MockPolicyEvaluationContext(attributeService);
+		this.currentPolicy = createStrictMock(Policy.class);
+		EvaluationContextFactory f = new DefaultEvaluationContextFactory(attributeService);
+		this.context = f.createContext(currentPolicy);
 	}
 }
