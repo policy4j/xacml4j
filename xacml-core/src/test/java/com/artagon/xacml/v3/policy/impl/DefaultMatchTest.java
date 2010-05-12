42a1daa50287cc78deb7fb525f0a2e3bb78565b8
Valdas Sevelis <valdas.s@gmail.com>
2010-05-12 19:24:06 +0000

    Fixed easymock reference bug

--- xacml-core/src/test/java/com/artagon/xacml/v3/policy/impl/DefaultMatchTest.java
+++ xacml-core/src/test/java/com/artagon/xacml/v3/policy/impl/DefaultMatchTest.java
@@ -11,6 +11,7 @@ import org.junit.Test;
 
 import com.artagon.xacml.v3.policy.AttributeDesignator;
 import com.artagon.xacml.v3.policy.AttributeReferenceEvaluationException;
+import com.artagon.xacml.v3.policy.AttributeValue;
 import com.artagon.xacml.v3.policy.EvaluationContext;
 import com.artagon.xacml.v3.policy.EvaluationException;
 import com.artagon.xacml.v3.policy.FunctionSpec;
@@ -36,13 +37,16 @@ public class DefaultMatchTest extends XacmlPolicyTestCase
 	@Test
 	public void testMatchEvaluation() throws EvaluationException
 	{
+		AttributeValue int1 = DataTypes.INTEGER.create(1);
+		AttributeValue int2 = DataTypes.INTEGER.create(2);
+		
 		expect(spec.getNumberOfParams()).andReturn(2);
 		expect(spec.getParamSpecAt(0)).andReturn(new ParamValueTypeSpec(DataTypes.INTEGER.getType()));
 		expect(spec.getParamSpecAt(1)).andReturn(new ParamValueTypeSpec(DataTypes.INTEGER.getType()));
 		expect(ref.getDataType()).andReturn(DataTypes.INTEGER.getType());
-		expect(ref.evaluate(context)).andReturn(DataTypes.INTEGER.bag(DataTypes.INTEGER.create(2), DataTypes.INTEGER.create(1)));
-		expect(spec.invoke(context, DataTypes.INTEGER.create(1), DataTypes.INTEGER.create(2))).andReturn(DataTypes.BOOLEAN.create(false));
-		expect(spec.invoke(context, DataTypes.INTEGER.create(1), DataTypes.INTEGER.create(1))).andReturn(DataTypes.BOOLEAN.create(true));
+		expect(ref.evaluate(context)).andReturn(DataTypes.INTEGER.bag(int2, int1));
+		expect(spec.invoke(context, int1, int2)).andReturn(DataTypes.BOOLEAN.create(false));
+		expect(spec.invoke(context, int1, int1)).andReturn(DataTypes.BOOLEAN.create(true));
 		replay(spec, ref);
 		Match m = new DefaultMatch(spec, DataTypes.INTEGER.create(1), ref);
 		assertEquals(MatchResult.MATCH, m.match(context));
