e4cfb4295f0e00c6925f131714e9ef945ab5362d
Giedrius Trumpickas <trumpyla@artagon.com>
2010-08-19 02:08:58 +0000

    Fixed protoc plugin issues

---
+++ xacml-protobuf/src/test/java/com/artagon/xacml/v30/protobuf/TestXacmlProtobuf.java
@@ -0,0 +1,15 @@
+package com.artagon.xacml.v30.protobuf;
+
+import org.junit.Test;
+
+import com.artagon.xacml.v30.protbuf.Xacml30;
+import com.artagon.xacml.v30.protbuf.Xacml30.Attribute;
+
+public class TestXacmlProtobuf 
+{
+	@Test
+	public void test()
+	{
+		
+	}
+}
