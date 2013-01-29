74d09876e2ddb5bf09c42447af72815d61c71a02
Giedrius Trumpickas <trumpyla@gmail.com>
2013-01-28 19:31:16 -0500

    Fixes

--- xacml-core/src/main/java/com/artagon/xacml/v30/marshall/jaxb/builder/ExpressionTypeBuilder.java
+++ xacml-core/src/main/java/com/artagon/xacml/v30/marshall/jaxb/builder/ExpressionTypeBuilder.java
@@ -7,23 +7,80 @@ import javax.xml.bind.JAXBElement;
 
 import org.oasis.xacml.v30.jaxb.ApplyType;
 import org.oasis.xacml.v30.jaxb.AttributeDesignatorType;
-import org.oasis.xacml.v30.jaxb.ExpressionType;
+import org.oasis.xacml.v30.jaxb.AttributeSelectorType;
+import org.oasis.xacml.v30.jaxb.AttributeValueType;
+import org.oasis.xacml.v30.jaxb.FunctionType;
+import org.oasis.xacml.v30.jaxb.ObjectFactory;
 import org.oasis.xacml.v30.jaxb.VariableReferenceType;
 
+import com.artagon.xacml.v30.AttributeDesignatorKey;
+import com.artagon.xacml.v30.AttributeExp;
+import com.artagon.xacml.v30.AttributeSelectorKey;
+import com.artagon.xacml.v30.Expression;
 import com.artagon.xacml.v30.pdp.Apply;
 import com.artagon.xacml.v30.pdp.AttributeDesignator;
+import com.artagon.xacml.v30.pdp.AttributeSelector;
+import com.artagon.xacml.v30.pdp.FunctionReference;
 import com.artagon.xacml.v30.pdp.VariableReference;
 import com.google.common.base.Preconditions;
-import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableMap;
 
-public interface ExpressionTypeBuilder
+public abstract class ExpressionTypeBuilder
 {
- 	ExpressionType build();
- 	JAXBElement<?> buildJaxbElement();
+	private final static ObjectFactory factory = new ObjectFactory();
+	private static ImmutableMap<Class<? extends Expression>, Maker> builders;
+	
+	public abstract void from(Expression e);
+ 	public abstract JAXBElement<?> build();
+ 	
+ 	static
+ 	{
+ 		ImmutableMap.Builder<Class<? extends Expression>, Maker> b = ImmutableMap.builder();
+ 		b.put(Apply.class, new Maker(){
+			@Override
+			public ExpressionTypeBuilder make() {
+				return new ApplyTypeBuilder();
+			}
+ 		});
+ 		b.put(AttributeDesignator.class, new Maker(){
+			@Override
+			public ExpressionTypeBuilder make() {
+				return new AttributeDesignatorTypeBuilder();
+			}
+ 		});
+ 		b.put(AttributeSelector.class, new Maker(){
+			@Override
+			public ExpressionTypeBuilder make() {
+				return new AttributeSelectorTypeBuilder();
+			}
+ 		});
+ 		b.put(VariableReference.class, new Maker(){
+			@Override
+			public ExpressionTypeBuilder make() {
+				return new VariableReferenceTypeBuilder();
+			}
+ 		});
+ 		b.put(FunctionReference.class, new Maker(){
+			@Override
+			public ExpressionTypeBuilder make() {
+				return new FunctionTypeBuilder();
+			}
+ 		});
+ 		b.put(AttributeExp.class, new Maker(){
+			@Override
+			public ExpressionTypeBuilder make() {
+				return new AttributeValueTypeBuilder();
+			}
+ 		});
+ 		builders = b.build();
+ 	}
+ 	
+ 	public static ExpressionTypeBuilder getBuilder(Expression exp){
+ 		Maker m = builders.get(exp);
+ 		return (m != null)?m.make():null;
+ 	}
 
-
- 	public class ApplyTypeBuilder extends JAXBObjectBuilder
- 		implements ExpressionTypeBuilder
+ 	static class ApplyTypeBuilder extends ExpressionTypeBuilder
  	{
  		private String functionId;
  		private List<JAXBElement<?>> params = new LinkedList<JAXBElement<?>>();
@@ -31,148 +88,157 @@ public interface ExpressionTypeBuilder
  		private ApplyTypeBuilder(){
  		}
 
- 		public ApplyTypeBuilder from(Apply apply){
- 			this.functionId = apply.getFunctionId();
- 			return this;
- 		}
-
- 		public ApplyTypeBuilder functionId(String functionId){
- 			Preconditions.checkArgument(!Strings.isNullOrEmpty(functionId));
- 			this.functionId = functionId;
- 			return this;
- 		}
-
- 		public static ApplyTypeBuilder newBuilder(){
- 			return new ApplyTypeBuilder();
- 		}
-
- 		public ApplyTypeBuilder expression(ExpressionTypeBuilder exp)
+ 		@Override
+		public void from(Expression e)
  		{
- 			JAXBElement<?> p = exp.buildJaxbElement();
- 			Preconditions.checkState(p != null && p.getValue() != null);
- 			this.params.add(p);
- 			return this;
+ 			Preconditions.checkArgument(e instanceof Apply);
+ 			Apply apply = (Apply)e;
+ 			this.functionId = apply.getFunctionId();
+ 			for(Expression exp : apply.getArguments()){
+ 				ExpressionTypeBuilder builder = getBuilder(exp);
+ 				builder.from(exp);
+ 				params.add(builder.build());
+ 			}
  		}
 
 		@Override
-		public ApplyType build() {
+		public JAXBElement<?> build() {
 			ApplyType exp = new ApplyType();
 			exp.setFunctionId(functionId);
 			exp.getExpression().addAll(params);
-			return exp;
-		}
-
-		public JAXBElement<?> buildJaxbElement(){
-			return factory.createApply(build());
+			return factory.createApply(exp);
 		}
  	}
-	public class VariableReferenceTypeBuilder extends JAXBObjectBuilder
-			implements ExpressionTypeBuilder
+	
+ 	static class VariableReferenceTypeBuilder extends ExpressionTypeBuilder
  	{
  		private String variableId;
 
  		private VariableReferenceTypeBuilder(){
  		}
 
- 		public VariableReferenceTypeBuilder variableId(String variableId){
- 			Preconditions.checkArgument(!Strings.isNullOrEmpty(variableId));
- 			this.variableId = variableId;
- 			return this;
- 		}
-
- 		public static VariableReferenceTypeBuilder newBuilder(){
- 			return new VariableReferenceTypeBuilder();
- 		}
-
- 		public static VariableReferenceTypeBuilder newBuilder(VariableReference v){
- 			return new VariableReferenceTypeBuilder()
- 			.variableId(v.getVariableId());
+ 		@Override
+		public void from(Expression e){
+ 			Preconditions.checkArgument(e instanceof VariableReference);
+ 			VariableReference ref = (VariableReference)e;
+ 			this.variableId = ref.getVariableId();
  		}
 
 		@Override
-		public VariableReferenceType build() {
+		public JAXBElement<?> build() {
 			VariableReferenceType exp = new VariableReferenceType();
-			Preconditions.checkState(variableId != null, "Variable reference identifier can't be null");
 			exp.setVariableId(variableId);
-			return exp;
+			return factory.createVariableReference(exp);
 		}
+ 	}
+ 	
+ 	public static class FunctionTypeBuilder extends ExpressionTypeBuilder
+ 	{
+ 		private String functionId;
 
-		public JAXBElement<?> buildJaxbElement(){
-			return factory.createVariableReference(build());
+ 		@Override
+		public void from(Expression e){
+ 			Preconditions.checkArgument(e instanceof FunctionReference);
+ 			FunctionReference ref = (FunctionReference)e;
+ 			this.functionId = ref.getFunctionId();
+ 		}
+
+		@Override
+		public JAXBElement<?> build() {
+			FunctionType exp = new FunctionType();
+			exp.setFunctionId(functionId);
+			return factory.createFunction(exp);
+		}
+ 	}
+ 	
+ 	static class AttributeValueTypeBuilder extends ExpressionTypeBuilder
+ 	{
+ 		private String dataTypeId;
+ 		private String value;
+
+ 		@Override
+		public void from(Expression e){
+ 			Preconditions.checkArgument(e instanceof AttributeExp);
+ 			AttributeExp v = (AttributeExp)e;
+ 			this.dataTypeId = v.getType().getDataTypeId();
+ 			this.value = v.toXacmlString();
+ 		}
+
+		@Override
+		public JAXBElement<?> build() {
+			AttributeValueType exp = new AttributeValueType();
+			exp.setDataType(dataTypeId);
+			exp.getContent().add(value);
+			return factory.createAttributeValue(exp);
 		}
  	}
 
-	public class AttributeDesignatorTypeBuilder extends JAXBObjectBuilder
-		implements ExpressionTypeBuilder
+	static class AttributeDesignatorTypeBuilder extends ExpressionTypeBuilder
 	{
 		private String attributeId;
 		private String category;
 		private String issuer;
 		private String dataType;
 		private boolean mustBePresent = false;
-
-		private AttributeDesignatorTypeBuilder(){
-
-		}
-
-		public AttributeDesignatorTypeBuilder attributeId(String attributeId){
-			Preconditions.checkArgument(!Strings.isNullOrEmpty(attributeId));
-			this.attributeId = attributeId;
-			return this;
-		}
-
-		public AttributeDesignatorTypeBuilder category(String category){
-			Preconditions.checkArgument(!Strings.isNullOrEmpty(category));
-			this.category = category;
-			return this;
-		}
-
-		public AttributeDesignatorTypeBuilder dataType(String dataType){
-			Preconditions.checkArgument(!Strings.isNullOrEmpty(dataType));
-			this.dataType = dataType;
-			return this;
-		}
-
-		public AttributeDesignatorTypeBuilder issuer(String issuer){
-			Preconditions.checkArgument(!Strings.isNullOrEmpty(issuer));
-			this.issuer = issuer;
-			return this;
-		}
-
-		public AttributeDesignatorTypeBuilder mustBePresent(boolean mustBePresent){
-			this.mustBePresent = mustBePresent;
-			return this;
-		}
-
-		public static AttributeDesignatorTypeBuilder newBuilder(){
-			return new AttributeDesignatorTypeBuilder();
-		}
-
-		public static AttributeDesignatorTypeBuilder newBuilder(AttributeDesignator d){
-			return new AttributeDesignatorTypeBuilder()
-			.attributeId(d.getReferenceKey().getAttributeId())
-			.category(d.getReferenceKey().getCategory().toString())
-			.dataType(d.getReferenceKey().getDataType().getDataTypeId())
-			.issuer(d.getReferenceKey().getIssuer())
-			.mustBePresent(d.isMustBePresent());
+		
+		@Override
+		public void from(Expression e){
+			Preconditions.checkArgument(e instanceof AttributeDesignator);
+			AttributeDesignator d = (AttributeDesignator)e;
+			AttributeDesignatorKey k = d.getReferenceKey();
+			this.attributeId = k.getAttributeId();
+			this.category = k.getCategory().getId();
+			this.dataType = k.getDataType().getDataTypeId();
+			this.issuer = k.getIssuer();
+			this.mustBePresent = d.isMustBePresent();
 		}
 
 		@Override
-		public AttributeDesignatorType build() {
+		public JAXBElement<?> build() {
 			AttributeDesignatorType exp = new AttributeDesignatorType();
-			Preconditions.checkState(attributeId != null, "Attribute identifier can't be null");
 			exp.setAttributeId(attributeId);
-			Preconditions.checkState(category != null, "Attribute category can't be null");
 			exp.setCategory(category);
 			exp.setIssuer(issuer);
-			Preconditions.checkState(dataType != null, "Attribute dataType can't be null");
 			exp.setDataType(dataType);
 			exp.setMustBePresent(mustBePresent);
-			return exp;
+			return factory.createAttributeDesignator(exp);
+		}
+	}
+	
+	public static class AttributeSelectorTypeBuilder extends ExpressionTypeBuilder
+	{
+		private String xpath;
+		private String category;
+		private String contextSelectorId;
+		private String dataType;
+		private boolean mustBePresent = false;
+		
+		@Override
+		public void from(Expression e){
+			Preconditions.checkArgument(e instanceof AttributeSelector);
+			AttributeSelector s = (AttributeSelector)e;
+			AttributeSelectorKey k = s.getReferenceKey();
+			this.xpath = k.getPath();
+			this.category = k.getCategory().getId();
+			this.dataType = k.getDataType().getDataTypeId();
+			this.contextSelectorId = k.getContextSelectorId();
+			this.mustBePresent = s.isMustBePresent();
 		}
 
-		public JAXBElement<?> buildJaxbElement(){
-			return factory.createAttributeDesignator(build());
+		@Override
+		public JAXBElement<?> build() {
+			AttributeSelectorType exp = new AttributeSelectorType();
+			exp.setPath(xpath);
+			exp.setCategory(category);
+			exp.setDataType(dataType);
+			exp.setMustBePresent(mustBePresent);
+			exp.setContextSelectorId(contextSelectorId);
+			return factory.createAttributeSelector(exp);
 		}
+	}
+	
+	public interface Maker
+	{
+		ExpressionTypeBuilder make();
 	}
 }
