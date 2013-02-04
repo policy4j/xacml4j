5d5fa39b263559e7fd5ff19ba60b08ad54394cb3
Giedrius Trumpickas <trumpyla@gmail.com>
2013-02-04 13:43:28 -0500

    Removed OpenSAML module

--- xacml-opensaml/src/main/java/org/xacml4j/opensaml/OpenSamlEndpoint.java
+++
@@ -1,9 +0,0 @@
-package org.xacml4j.opensaml;
-
-import org.opensaml.saml2.core.RequestAbstractType;
-import org.opensaml.saml2.core.Response;
-
-public interface OpenSamlEndpoint 
-{
-	Response handle(RequestAbstractType req);
-}
