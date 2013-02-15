b43d151e273c9f402d223cfed49992984ad04202
Rolandas Valteris <rolandas.valteris@gmail.com>
2013-02-15 16:22:26 -0500

    fixed resource property names

--- xacml-json/src/main/java/org/xacml4j/v30/marshal/json/RequestContextAdapter.java
+++ xacml-json/src/main/java/org/xacml4j/v30/marshal/json/RequestContextAdapter.java
@@ -16,31 +16,36 @@ import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
 import com.google.gson.reflect.TypeToken;
 
-final class RequestContextAdapter implements JsonDeserializer<RequestContext>, JsonSerializer<RequestContext>
-{
+final class RequestContextAdapter implements JsonDeserializer<RequestContext>, JsonSerializer<RequestContext> {
+
+	private static final String RETURN_POLICY_ID_LIST_PROPERTY = "ReturnPolicyIdList";
+	private static final String COMBINED_DECISION_PROPERTY = "CombinedDecision";
+	private static final String ATTRIBUTES_PROPERTY = "Attributes";
+	private static final String MULTI_REQUESTS_PROPERTY = "MultiRequests";
 
 	@Override
-	public RequestContext deserialize(JsonElement json, Type typeOfT,
-			JsonDeserializationContext context) throws JsonParseException {
+	public RequestContext deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+			throws JsonParseException {
 		JsonObject o = json.getAsJsonObject();
-		boolean returnPolicyIdList = GsonUtil.getAsBoolean(o, "returnPolicyIdList", false);
-		boolean combinedDecision = GsonUtil.getAsBoolean(o, "combinedDecision", false);
-		Collection<Attributes> attributes = context.deserialize(o.getAsJsonArray("attributes"), new TypeToken<Collection<Attributes>>(){}.getType());
-		Collection<RequestReference> reqRef = context.deserialize(o.getAsJsonArray("multipleRequests"), new TypeToken<Collection<RequestReference>>(){}.getType());
+		boolean returnPolicyIdList = GsonUtil.getAsBoolean(o, RETURN_POLICY_ID_LIST_PROPERTY, false);
+		boolean combinedDecision = GsonUtil.getAsBoolean(o, COMBINED_DECISION_PROPERTY, false);
+		Collection<Attributes> attributes = context.deserialize(o.getAsJsonArray(ATTRIBUTES_PROPERTY),
+				new TypeToken<Collection<Attributes>>() {
+				}.getType());
+		Collection<RequestReference> reqRef = context.deserialize(o.getAsJsonArray(MULTI_REQUESTS_PROPERTY),
+				new TypeToken<Collection<RequestReference>>() {
+				}.getType());
 		return new RequestContext(returnPolicyIdList, combinedDecision, attributes, reqRef);
 	}
 
 	@Override
-	public JsonElement serialize(RequestContext src, Type typeOfSrc,
-			JsonSerializationContext context) {
-		
+	public JsonElement serialize(RequestContext src, Type typeOfSrc, JsonSerializationContext context) {
 		JsonObject o = new JsonObject();
-		o.addProperty("returnPolicyIdList", src.isReturnPolicyIdList());
-		o.addProperty("combinedDecision", src.isCombinedDecision());
-		o.add("attributes", context.serialize(src.getAttributes()));
-		o.add("multipleRequests", context.serialize(src.getRequestReferences()));
+		o.addProperty(RETURN_POLICY_ID_LIST_PROPERTY, src.isReturnPolicyIdList());
+		o.addProperty(COMBINED_DECISION_PROPERTY, src.isCombinedDecision());
+		o.add(ATTRIBUTES_PROPERTY, context.serialize(src.getAttributes()));
+		o.add(MULTI_REQUESTS_PROPERTY, context.serialize(src.getRequestReferences()));
 		return o;
 	}
-	
-	
+
 }
