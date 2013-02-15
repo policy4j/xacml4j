f5d501c537c25d8afb1f33271bafeb1c77f9daba
Rolandas Valteris <rolandas.valteris@gmail.com>
2013-02-15 17:50:51 -0500

    fixed Attributes JSON property names

--- xacml-json/src/main/java/org/xacml4j/v30/marshal/json/AttributesAdapter.java
+++ xacml-json/src/main/java/org/xacml4j/v30/marshal/json/AttributesAdapter.java
@@ -17,33 +17,36 @@ import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
 import com.google.gson.reflect.TypeToken;
 
-class AttributesAdapter implements JsonDeserializer<Attributes>, JsonSerializer<Attributes>
-{
+class AttributesAdapter implements JsonDeserializer<Attributes>, JsonSerializer<Attributes> {
+	private static final String CATEGORY_PROPERTY = "Category";
+	private static final String ID_PROPERTY = "Id";
+	private static final String CONTENT_PROPERTY = "Content";
+	private static final String ATTRIBUTE_PROPERTY = "Attribute";
+
 	@Override
-	public Attributes deserialize(JsonElement json, Type typeOfT,
-			JsonDeserializationContext context) throws JsonParseException {
-		try{
+	public Attributes deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+			throws JsonParseException {
+		try {
 			JsonObject o = json.getAsJsonObject();
-			Collection<Attribute> attr = context.deserialize(o.getAsJsonArray("attributes"), new TypeToken<Collection<Attribute>>(){}.getType());
-			return Attributes
-					.builder( AttributeCategories.parse(GsonUtil.getAsString(o, "category", null)))
-					.id(GsonUtil.getAsString(o, "id", null))
-					.attributes(attr)
-					.build();
-		}catch(XacmlSyntaxException e){
+			Collection<Attribute> attr = context.deserialize(o.getAsJsonArray(ATTRIBUTE_PROPERTY),
+					new TypeToken<Collection<Attribute>>() {
+					}.getType());
+			return Attributes.builder(AttributeCategories.parse(GsonUtil.getAsString(o, CATEGORY_PROPERTY, null)))
+					.id(GsonUtil.getAsString(o, ID_PROPERTY, null)).attributes(attr).build();
+		} catch (XacmlSyntaxException e) {
 			throw new JsonParseException(e);
 		}
 	}
 
 	@Override
-	public JsonElement serialize(Attributes src, Type typeOfSrc,
-			JsonSerializationContext context) {
+	public JsonElement serialize(Attributes src, Type typeOfSrc, JsonSerializationContext context) {
 		JsonObject o = new JsonObject();
-		if(src.getId() != null){
-			o.addProperty("id", src.getId());
+		if (src.getId() != null) {
+			o.addProperty(ID_PROPERTY, src.getId());
 		}
-		o.addProperty("category", src.getCategory().getId());
-		o.add("attributes", context.serialize(src.getAttributes()));
+		o.addProperty(CATEGORY_PROPERTY, src.getCategory().getId());
+		o.add(ATTRIBUTE_PROPERTY, context.serialize(src.getAttributes()));
 		return o;
 	}
+
 }
