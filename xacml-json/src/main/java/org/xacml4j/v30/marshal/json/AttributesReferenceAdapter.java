1319d2469d8722e1c2762192899f490279dcddb4
Rolandas Valteris <rolandas.valteris@gmail.com>
2013-03-05 02:02:45 -0500

    fixed AttributesReference de/serialization

--- xacml-json/src/main/java/org/xacml4j/v30/marshal/json/AttributesReferenceAdapter.java
+++ xacml-json/src/main/java/org/xacml4j/v30/marshal/json/AttributesReferenceAdapter.java
@@ -4,35 +4,26 @@ import java.lang.reflect.Type;
 
 import org.xacml4j.v30.AttributesReference;
 
-import com.google.common.base.Preconditions;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
 
-final class AttributesRefererenceAdapater implements JsonDeserializer<AttributesReference>, JsonSerializer<AttributesReference>
-{
+final class AttributesRefererenceAdapater implements JsonDeserializer<AttributesReference>,
+		JsonSerializer<AttributesReference> {
 	@Override
-	public AttributesReference deserialize(JsonElement json, Type typeOfT,
-			JsonDeserializationContext context) throws JsonParseException {
-		JsonObject ref = json.getAsJsonObject();
-		JsonElement element = ref.get("$ref");
-		Preconditions.checkNotNull(element);
-		return AttributesReference
-				.builder()
-				.id(ref.get("$ref").getAsString())
-				.build();
+	public AttributesReference deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+			throws JsonParseException {
+		String referenceId = json.getAsJsonPrimitive().getAsString();
+		return AttributesReference.builder().id(referenceId).build();
 	}
 
 	@Override
-	public JsonElement serialize(AttributesReference src, Type typeOfSrc,
-			JsonSerializationContext context) {
-		JsonObject o = new JsonObject();
-		o.addProperty("$ref", src.getReferenceId());
-		return o;
+	public JsonElement serialize(AttributesReference src, Type typeOfSrc, JsonSerializationContext context) {
+		return new JsonPrimitive(src.getReferenceId());
 	}
+
 }
-
