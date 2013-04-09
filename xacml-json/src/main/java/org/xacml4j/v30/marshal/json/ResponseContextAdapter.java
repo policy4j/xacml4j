131f7f1849179a9081e0467827a4b4c75bfa4636
Rolandas Valteris <rolandas.valteris@gmail.com>
2013-04-09 14:26:26 -0400

    added ResponseContext serialization

--- xacml-json/src/main/java/org/xacml4j/v30/marshal/json/ResponseContextAdapter.java
+++ xacml-json/src/main/java/org/xacml4j/v30/marshal/json/ResponseContextAdapter.java
@@ -11,16 +11,29 @@ import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
 import com.google.gson.reflect.TypeToken;
 
-public class ResponseContextAdapter implements JsonDeserializer<ResponseContext> {
+public class ResponseContextAdapter implements JsonDeserializer<ResponseContext>, JsonSerializer<ResponseContext> {
+	private static final String RESULT_PROPERTY = "Result";
+
+	private static final Type RESULTS_TYPE = new TypeToken<Collection<Result>>() {
+	}.getType();
+
 	@Override
 	public ResponseContext deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
 			throws JsonParseException {
 		JsonObject o = json.getAsJsonObject();
-		Collection<Result> r = context.deserialize(o.getAsJsonArray("Result"), new TypeToken<Collection<Result>>() {
-		}.getType());
+		Collection<Result> r = context.deserialize(o.get(RESULT_PROPERTY), RESULTS_TYPE);
 		return ResponseContext.builder().results(r).build();
+	}
+
+	@Override
+	public JsonElement serialize(ResponseContext src, Type typeOfSrc, JsonSerializationContext context) {
+		JsonObject o = new JsonObject();
+		o.add(RESULT_PROPERTY, context.serialize(src.getResults()));
+		return o;
 	}
 
 }
