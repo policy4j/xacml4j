327bb9be4357b8d133d4f6ca6fbdfe2cd6a0cfbc
Rolandas Valteris <rolandas.valteris@gmail.com>
2013-04-10 13:22:16 -0400

    added AttributeAssignment serialization

--- xacml-json/src/main/java/org/xacml4j/v30/marshal/json/AttributeAssignmentSerializer.java
+++ xacml-json/src/main/java/org/xacml4j/v30/marshal/json/AttributeAssignmentSerializer.java
@@ -1,10 +1,17 @@
 package org.xacml4j.v30.marshal.json;
 
+import static org.xacml4j.v30.marshal.json.AttributeProperties.ATTRIBUTE_ID_PROPERTY;
+import static org.xacml4j.v30.marshal.json.AttributeProperties.DATA_TYPE_PROPERTY;
+import static org.xacml4j.v30.marshal.json.AttributeProperties.ISSUER_PROPERTY;
+import static org.xacml4j.v30.marshal.json.AttributeProperties.VALUE_PROPERTY;
+
 import java.lang.reflect.Type;
 
 import org.xacml4j.v30.AttributeAssignment;
+import org.xacml4j.v30.AttributeExp;
 
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
 
@@ -12,8 +19,23 @@ public class AttributeAssignmentSerializer implements JsonSerializer<AttributeAs
 
 	@Override
 	public JsonElement serialize(AttributeAssignment src, Type typeOfSrc, JsonSerializationContext context) {
-		// TODO Auto-generated method stub
-		return null;
+		JsonObject o = new JsonObject();
+		o.addProperty(ATTRIBUTE_ID_PROPERTY, src.getAttributeId());
+
+		AttributeExp value = src.getAttribute();
+		o.add(VALUE_PROPERTY, context.serialize(value, AttributeExp.class));
+		if (value.getType() != null) {
+			o.addProperty(DATA_TYPE_PROPERTY, value.getType().getDataTypeId());
+		}
+
+		if (src.getCategory() != null) {
+			o.addProperty("Category", src.getCategory().toString());
+		}
+		if (src.getIssuer() != null) {
+			o.addProperty(ISSUER_PROPERTY, src.getIssuer());
+		}
+
+		return o;
 	}
 
 }
