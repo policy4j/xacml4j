e7b7df560ee3fe155e2f1aeb6d8410e40bfba803
Valdas Sevelis <valdas.s@gmail.com>
2014-07-04 16:10:24 -0400

    Updated Internet Interface test

--- xacml-core/src/test/java/org/xacml4j/v30/EUI48Test.java
+++ xacml-core/src/test/java/org/xacml4j/v30/EUI48Test.java
@@ -10,12 +10,12 @@ package org.xacml4j.v30;
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
@@ -1131,7 +1131,8 @@ public class EUI48Test {
 	public void testFromInterface() throws Exception {
 		boolean loopbacksOnly = true;
 		for(Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
-			loopbacksOnly &= en.nextElement().isLoopback();
+			NetworkInterface ni = en.nextElement();
+			loopbacksOnly &= ni.isLoopback() || ni.getHardwareAddress() == null;
 		}
 
 		if (!loopbacksOnly) {
