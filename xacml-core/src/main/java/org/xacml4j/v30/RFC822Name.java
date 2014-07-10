f6a7b719b54eca42427b8dd2ebb802d3725fe461
Valdas Sevelis <valdas.s@gmail.com>
2014-07-10 09:26:53 -0400

    Cleanup exception processing in RFC822Name.java

--- xacml-core/src/main/java/org/xacml4j/v30/RFC822Name.java
+++ xacml-core/src/main/java/org/xacml4j/v30/RFC822Name.java
@@ -10,12 +10,12 @@ package org.xacml4j.v30;
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
@@ -161,7 +161,6 @@ public final class RFC822Name implements Serializable
 			VALID_PATTERN = Pattern.compile(PATTERN_STRING);
 		} catch(PatternSyntaxException e) {
 			LoggerFactory.getLogger(RFC822Name.class).error("Can not parse Email address pattern", e);
-			e.printStackTrace();
 			throw e;
 		}
 	}
