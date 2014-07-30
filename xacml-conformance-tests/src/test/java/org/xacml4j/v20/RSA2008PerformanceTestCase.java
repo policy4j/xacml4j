8264555f979cb0c986c40bd9d85a75f7f24e2243
Valdas Sevelis <valdas.s@gmail.com>
2014-07-30 14:56:13 -0400

    Renamed RSA2008PerformanceTestCase.java to exclude it from regular unit tests

--- xacml-conformance-tests/src/test/java/org/xacml4j/v20/RSA2008PerformanceTestCase.java
+++ xacml-conformance-tests/src/test/java/org/xacml4j/v20/RSA2008PerformanceTestCase.java
@@ -10,12 +10,12 @@ package org.xacml4j.v20;
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
@@ -45,7 +45,7 @@ import org.xacml4j.v30.spi.repository.PolicyRepository;
 
 import com.google.common.base.Supplier;
 
-public class RSA2008PerfTest extends XacmlPolicyTestSupport
+public class RSA2008PerformanceTestCase extends XacmlPolicyTestSupport
 {
 	private static PolicyDecisionPoint pdp;
 	private static RequestContext[][] requests;
