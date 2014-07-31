a342752417c7019b8326dcda03c844219be3bca2
Valdas Sevelis <valdas.s@gmail.com>
2014-07-30 23:29:19 -0400

    'equals' method cleanup

--- xacml-core/src/main/java/org/xacml4j/v30/ResponseContext.java
+++ xacml-core/src/main/java/org/xacml4j/v30/ResponseContext.java
@@ -10,12 +10,12 @@ package org.xacml4j.v30;
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
@@ -30,7 +30,7 @@ import com.google.common.collect.ImmutableList;
 
 public class ResponseContext
 {
-	private Collection<Result> results;
+	private ImmutableList<Result> results;
 
 	private ResponseContext(Builder builder){
 		this.results = builder.b.build();
@@ -59,13 +59,10 @@ public class ResponseContext
 
 	@Override
 	public boolean equals(Object o){
-		if(o == this){
+		if (o == this) {
 			return true;
 		}
-		if(o == null){
-			return false;
-		}
-		if(!(o instanceof ResponseContext)){
+		if (!(o instanceof ResponseContext)) {
 			return false;
 		}
 		ResponseContext r = (ResponseContext)o;
