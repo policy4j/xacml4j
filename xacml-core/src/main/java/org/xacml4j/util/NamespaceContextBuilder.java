ac30da9de77d185540f6c85196ad45b05eb1ce1c
Giedrius Trumpickas <trumpyla@gmail.com>
2014-09-12 14:31:44 -0400

    NamespaceContextBuilder and some code cleanups

---
+++ xacml-core/src/main/java/org/xacml4j/util/NamespaceContextBuilder.java
@@ -0,0 +1,7 @@
+package org.xacml4j.util;
+
+/**
+ * Created by trumpyla on 9/9/14.
+ */
+public class NamespaceContextBuilder {
+}
---
+++ xacml-core/src/test/java/org/xacml4j/util/NamespaceContextBuilderTest.java
@@ -0,0 +1,7 @@
+package org.xacml4j.util;
+
+/**
+ * Created by trumpyla on 9/10/14.
+ */
+public class NamespaceContextBuilderTest {
+}
